e38c34d7814023a741fb12bb17a9bc5a5416d719
Mirko Rinelli <mirko.rinelli@gmail.com>
2020-11-19 16:34:34 +0100

    working on js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,17 @@
 //Scrivi un programma che stampi i numeri da 1 a 100.
-numberList = [];
-//Per i multipli di 3 stampi “Fizz” al posto del numero.
+for (i=1; i < 101; i++) {
+  console.log(i);
 
-//Per i multipli di 5 stampi Buzz.
+  //Per i multipli di 3 stampi “Fizz” al posto del numero.
+  if ((i % 3) === 0) {
+    i = "Fizz";
+    console.log(i);
 
+    //Per i multipli di 5 stampi Buzz.
+  } else if ((i % 5) === 0) {
+    i = "Buzz";
+    console.log(i);
+  } else {
+  }
+}
 //Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
